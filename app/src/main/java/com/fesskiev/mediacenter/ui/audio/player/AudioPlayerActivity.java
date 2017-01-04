ce64b86fa6a8f81056f58442bb4007973c4272f1
fesskiev <fesskiev@gmail.com>
2017-01-04 09:42:40 +0200

    add animation

--- app/src/main/java/com/fesskiev/mediacenter/ui/audio/player/AudioPlayerActivity.java
+++ app/src/main/java/com/fesskiev/mediacenter/ui/audio/player/AudioPlayerActivity.java
@@ -2,6 +2,7 @@ package com.fesskiev.mediacenter.ui.audio.player;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
@@ -74,7 +75,7 @@ public class AudioPlayerActivity extends AnalyticsActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         if (savedInstanceState == null) {
@@ -100,8 +101,18 @@ public class AudioPlayerActivity extends AnalyticsActivity {
         album = (TextView) findViewById(R.id.album);
         volumeLevel = (TextView) findViewById(R.id.volumeLevel);
 
-        findViewById(R.id.previousTrack).setOnClickListener(v -> previous());
-        findViewById(R.id.nextTrack).setOnClickListener(v -> next());
+        ImageView prevTrack = (ImageView) findViewById(R.id.previousTrack);
+        prevTrack.setOnClickListener(v -> {
+            ((Animatable) prevTrack.getDrawable()).start();
+            previous();
+        });
+
+        ImageView nextTrack = (ImageView) findViewById(R.id.nextTrack);
+        nextTrack.setOnClickListener(v -> {
+            ((Animatable) nextTrack.getDrawable()).start();
+            next();
+        });
+
         findViewById(R.id.equalizer).setOnClickListener(v -> startEqualizerActivity());
         findViewById(R.id.trackList).setOnClickListener(v -> openTrackList());
 
