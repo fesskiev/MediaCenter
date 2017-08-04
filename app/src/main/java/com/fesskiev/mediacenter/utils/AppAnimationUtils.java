f808fc18e684149d30230cc8a318612d15c9227d
fesskiev <fesskiev@gmail.com>
2017-08-04 18:23:55 +0300

    fix rotate animation

--- app/src/main/java/com/fesskiev/mediacenter/utils/AppAnimationUtils.java
+++ app/src/main/java/com/fesskiev/mediacenter/utils/AppAnimationUtils.java
@@ -138,7 +138,7 @@ public class AppAnimationUtils {
 
     public void rotateExpand(View view) {
         RotateAnimation rotate =
-                new RotateAnimation(360, 180, RELATIVE_TO_SELF,
+                new RotateAnimation(180, 360, RELATIVE_TO_SELF,
                         0.5f, RELATIVE_TO_SELF, 0.5f);
         rotate.setDuration(300);
         rotate.setFillAfter(true);
@@ -147,11 +147,12 @@ public class AppAnimationUtils {
 
     public void rotateCollapse(View view) {
         RotateAnimation rotate =
-                new RotateAnimation(180, 360, RELATIVE_TO_SELF,
+                new RotateAnimation(360, 180, RELATIVE_TO_SELF,
                         0.5f, RELATIVE_TO_SELF, 0.5f);
         rotate.setDuration(300);
         rotate.setFillAfter(true);
         view.setAnimation(rotate);
+
     }
 
 
