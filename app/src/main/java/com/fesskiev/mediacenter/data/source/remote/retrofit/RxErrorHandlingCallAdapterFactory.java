1fa5fe39475818ec47042c99966190cec20528c8
fesskiev <fesskiev@gmail.com>
2017-09-06 16:14:07 +0300

    add rx error handling adapter

--- app/src/main/java/com/fesskiev/mediacenter/data/source/remote/retrofit/RxErrorHandlingCallAdapterFactory.java
+++ app/src/main/java/com/fesskiev/mediacenter/data/source/remote/retrofit/RxErrorHandlingCallAdapterFactory.java
@@ -7,36 +7,40 @@ import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Single;
+import io.reactivex.functions.Function;
 import retrofit2.Call;
 import retrofit2.CallAdapter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.HttpException;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import io.reactivex.Observable;;
-import rx.functions.Func1;
+import retrofit2.adapter.rxjava2.HttpException;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 public class RxErrorHandlingCallAdapterFactory extends CallAdapter.Factory {
-    private final RxJavaCallAdapterFactory original;
+    private final RxJava2CallAdapterFactory original;
 
     private RxErrorHandlingCallAdapterFactory() {
-        original = RxJavaCallAdapterFactory.create();
+        original = RxJava2CallAdapterFactory.create();
     }
 
     public static CallAdapter.Factory create() {
         return new RxErrorHandlingCallAdapterFactory();
     }
 
+    @SuppressWarnings("NullableProblems")
     @Override
-    public CallAdapter<?> get(Type returnType, Annotation[] annotations, Retrofit retrofit) {
+    public CallAdapter get(Type returnType, Annotation[] annotations, Retrofit retrofit) {
         return new RxCallAdapterWrapper(retrofit, original.get(returnType, annotations, retrofit));
     }
 
-    private static class RxCallAdapterWrapper implements CallAdapter<Observable<?>> {
+    private static class RxCallAdapterWrapper implements CallAdapter {
         private final Retrofit retrofit;
-        private final CallAdapter<?> wrapped;
+        private final CallAdapter wrapped;
 
-        public RxCallAdapterWrapper(Retrofit retrofit, CallAdapter<?> wrapped) {
+        public RxCallAdapterWrapper(Retrofit retrofit, CallAdapter wrapped) {
             this.retrofit = retrofit;
             this.wrapped = wrapped;
         }
@@ -46,13 +50,28 @@ public class RxErrorHandlingCallAdapterFactory extends CallAdapter.Factory {
             return wrapped.responseType();
         }
 
-        @SuppressWarnings("unchecked")
+        @SuppressWarnings({"unchecked", "NullableProblems"})
         @Override
-        public <R> Observable<?> adapt(Call<R> call) {
-            return ((Observable) wrapped.adapt(call))
-                    .onErrorResumeNext(throwable -> {
-                        return Observable.error(asRetrofitException((Throwable) throwable));
-                    });
+        public Object adapt(Call call) {
+            Object adaptedCall = wrapped.adapt(call);
+
+            if (adaptedCall instanceof Completable) {
+                return ((Completable) adaptedCall).onErrorResumeNext(
+                        throwable -> Completable.error(asRetrofitException(throwable)));
+            }
+
+            if (adaptedCall instanceof Single) {
+                return ((Single) adaptedCall).onErrorResumeNext(
+                        throwable -> Single.error(asRetrofitException((Throwable) throwable)));
+            }
+
+            if (adaptedCall instanceof Observable) {
+                return ((Observable) adaptedCall).onErrorResumeNext(
+                        (Function<? super Throwable, ? extends ObservableSource>)
+                                throwable -> Observable.error(asRetrofitException(throwable)));
+            }
+
+            throw new RuntimeException("Observable Type not supported");
         }
 
         private RetrofitException asRetrofitException(Throwable throwable) {
